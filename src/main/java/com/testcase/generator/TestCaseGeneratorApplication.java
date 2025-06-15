8a5a6ec0e7ec5fd24fe7a14cebb9da09fdbc9832
akshaybabbar <akshaybabbar12@gmail.com>
2025-06-16 01:34:04 +0530

    update

--- src/main/java/com/testcase/generator/TestCaseGeneratorApplication.java
+++ src/main/java/com/testcase/generator/TestCaseGeneratorApplication.java
@@ -1,11 +1,23 @@
 package com.testcase.generator;
 
+import org.apache.catalina.valves.RemoteIpValve;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TestCaseGeneratorApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestCaseGeneratorApplication.class, args);
+    }
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        RemoteIpValve remoteIpValve = new RemoteIpValve();
+        remoteIpValve.setRemoteIpHeader("x-forwarded-for");
+        remoteIpValve.setProtocolHeader("x-forwarded-proto");
+        tomcat.addEngineValves(remoteIpValve);
+        return tomcat;
     }
 }
